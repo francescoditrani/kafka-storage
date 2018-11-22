26f500fa586969bac07fa5b1d93e4fe838fc678f
ankushkhanna <ankushkhanna22@gmail.com>
2018-11-22 21:05:21 +0100

    Getting it compiled.

--- src/main/scala/org/ditank/kafka/storage/RichKStreamsBuilder.scala
+++ src/main/scala/org/ditank/kafka/storage/RichKStreamsBuilder.scala
@@ -1,20 +1,27 @@
 package org.ditank.kafka.storage
 
-import org.apache.kafka.streams.kstream.Materialized
+import org.apache.avro.specific.SpecificRecord
+import org.apache.kafka.streams.kstream.{Consumed, Materialized}
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.state.Stores
+import org.ditank.kafka.storage.SerdeHelper._
 
 object RichKStreamsBuilder {
 
-  implicit class StoragedKStreamsBuilder(builder: StreamsBuilder) {
+  implicit class StorageKStreamsBuilder(builder: StreamsBuilder) {
 
-    def storageBuilder(): GKStreamsStorageBuilder = {
+    def storageBuilder[K <: SpecificRecord, V <: SpecificRecord](kafkaConf: KafkaConfiguration): GKStreamsStorageBuilder = {
+
+      implicit val consumed: Consumed[K, V] = Consumed.`with`(
+        createSerde[K](true, kafkaConf.schemaRegistryUrl),
+        createSerde[V](false, kafkaConf.schemaRegistryUrl)
+      )
 
       val mat = Materialized.as[K, V](Stores.persistentKeyValueStore(kafkaConf.streams.storeName))
-        .withKeySerde(streamSerdes.latestPartitionOffsetKey)
-        .withValueSerde(streamSerdes.latestPartitionOffset)
+        .withKeySerde(createSerde[K](true, kafkaConf.schemaRegistryUrl))
+        .withValueSerde(createSerde[V](false, kafkaConf.schemaRegistryUrl))
 
-      builder.globalTable(config.innerTopic, mat)
+      builder.globalTable(kafkaConf.innerTopic, mat)
 
       new GKStreamsStorageBuilder()
       //create store
