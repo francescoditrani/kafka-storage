52b0936c55a0e7256cd36f1c02dc9c158965c958
ankushkhanna <ankushkhanna22@gmail.com>
2018-11-23 10:40:45 +0100

    Changind ADT to return not future on get.

--- src/main/scala/org/ditank/kafka/storage/KafkaStorage.scala
+++ src/main/scala/org/ditank/kafka/storage/KafkaStorage.scala
@@ -9,15 +9,13 @@ trait KafkaStorage[K <: SpecificRecord, V <: SpecificRecord] {
 
   def insert(record: (K, V)): Future[Unit]
 
-  def get(key: K): Future[Option[V]]
+  def get(key: K): Option[V]
 
   def compareUpdate(key: K, updateWith: V => Option[V]): Future[Option[V]] = {
-    get(key)
-      .map(_.flatMap(updateWith))
-      .flatMap {
-        case Some(value) => insert((key, value)).map(_ => Some(value))
-        case None => Future(None)
-      }
+    get(key).flatMap[V](updateWith) match {
+      case Some(value) => insert((key, value)).map(_ => Some(value))
+      case None => Future(None)
+    }
   }
 
 }
