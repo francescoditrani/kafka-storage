264e3623e7db9894935f74bf1b6730851095b328
ankushkhanna <ankushkhanna22@gmail.com>
2018-11-23 11:21:10 +0100

    Fix some not pushed stuff.

--- src/main/scala/org/ditank/kafka/storage/GKStreamsStorageBuilder.scala
+++ src/main/scala/org/ditank/kafka/storage/GKStreamsStorageBuilder.scala
@@ -1,11 +1,29 @@
 package org.ditank.kafka.storage
 
+import java.util.Properties
+
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerializer
 import org.apache.avro.specific.SpecificRecord
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig}
 import org.apache.kafka.streams.KafkaStreams
 
 class GKStreamsStorageBuilder() {
 
-  def build[K <: SpecificRecord, V <: SpecificRecord](streams: KafkaStreams): KafkaStorage[K, V] =
-    new GKStreamsStorage(streams)
+  def build[K <: SpecificRecord, V <: SpecificRecord](streams: KafkaStreams, kafkaConfiguration: KafkaConfiguration): KafkaStorage[K, V] = {
+    val kafkaProducer = new KafkaProducer[K, V](conf(kafkaConfiguration))
+    new GKStreamsStorage(streams, kafkaProducer, kafkaConfiguration.innerTopic)
+  }
+
+  private def conf[K <: SpecificRecord, V <: SpecificRecord](kafkaConfiguration: KafkaConfiguration) = {
+    val config = new Properties()
+    config.put(ProducerConfig.CLIENT_ID_CONFIG, kafkaConfiguration.producer.clientId)
+    config.put(ProducerConfig.ACKS_CONFIG, "1")
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfiguration.bootstrapServer)
+    config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[SpecificAvroSerializer[K]])
+    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[SpecificAvroSerializer[V]])
+    config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, kafkaConfiguration.schemaRegistryUrl)
+    config
+  }
 
 }
