329c45791922bd702111861d5911545a350f2af8
ankush.khanna <ankush.khanna@flixbus.com>
2018-11-23 10:49:43 +0100

    Coverting Future into java Future.

--- src/main/scala/org/ditank/kafka/storage/KafkaStorage.scala
+++ src/main/scala/org/ditank/kafka/storage/KafkaStorage.scala
@@ -1,20 +1,21 @@
 package org.ditank.kafka.storage
 
-import org.apache.avro.specific.SpecificRecord
+import java.util.concurrent.CompletableFuture
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import org.apache.avro.specific.SpecificRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+
 
 trait KafkaStorage[K <: SpecificRecord, V <: SpecificRecord] {
 
-  def insert(record: (K, V)): Future[Unit]
+  def insert(record: (K, V)): java.util.concurrent.Future[RecordMetadata]
 
   def get(key: K): Option[V]
 
-  def compareUpdate(key: K, updateWith: V => Option[V]): Future[Option[V]] = {
-    get(key).flatMap[V](updateWith) match {
-      case Some(value) => insert((key, value)).map(_ => Some(value))
-      case None => Future(None)
+  def compareUpdate(key: K, updateWith: V => Option[V]): java.util.concurrent.Future[RecordMetadata] = {
+    get(key).flatMap(updateWith) match {
+      case Some(value) => insert((key, value))
+      case None => CompletableFuture.completedFuture(null)
     }
   }
 
