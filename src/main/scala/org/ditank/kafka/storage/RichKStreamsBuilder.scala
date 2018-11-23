61e098797d6227f16d8c20e9095ebedfc7a225c6
francescoditrani <f.ditrani@gmail.com>
2018-11-23 11:29:45 +0100

    Use implicit for Consumed

--- src/main/scala/org/ditank/kafka/storage/RichKStreamsBuilder.scala
+++ src/main/scala/org/ditank/kafka/storage/RichKStreamsBuilder.scala
@@ -1,7 +1,9 @@
 package org.ditank.kafka.storage
 
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde
 import org.apache.avro.specific.SpecificRecord
-import org.apache.kafka.streams.kstream.{Consumed, Materialized}
+import org.apache.kafka.streams.kstream.Materialized
+import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.state.Stores
 import org.ditank.kafka.storage.SerdeHelper._
@@ -12,10 +14,8 @@ object RichKStreamsBuilder {
 
     def storageBuilder[K <: SpecificRecord, V <: SpecificRecord](kafkaConf: KafkaConfiguration): GKStreamsStorageBuilder = {
 
-      implicit val consumed: Consumed[K, V] = Consumed.`with`(
-        createSerde[K](true, kafkaConf.schemaRegistryUrl),
-        createSerde[V](false, kafkaConf.schemaRegistryUrl)
-      )
+      implicit val keySerde: SpecificAvroSerde[K] = createSerde[K](true, kafkaConf.schemaRegistryUrl)
+      implicit val valueSerde: SpecificAvroSerde[V] = createSerde[V](false, kafkaConf.schemaRegistryUrl)
 
       val mat = Materialized.as[K, V](Stores.persistentKeyValueStore(kafkaConf.streams.storeName))
         .withKeySerde(createSerde[K](true, kafkaConf.schemaRegistryUrl))
@@ -24,9 +24,7 @@ object RichKStreamsBuilder {
       builder.globalTable(kafkaConf.innerTopic, mat)
 
       new GKStreamsStorageBuilder()
-      //create store
-      //attach the store
-      //we return the storageBuilder
+
     }
 
 
